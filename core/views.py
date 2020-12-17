cd7e5b6473e644e6039b36f5a6a1ccf747cf4fc2
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2020-12-16 20:46:09 -0800

    mod settings, added base/listing.html

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from core.models import Blog
 
-# Create your views here.
+def listing(request):
+    data = {
+        "blogs": Blog.objects.all(),
+    }
+
+    return render(request, "listing.html", data)
+
+def view_blog(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    data = {
+        "blog": blog,
+    }
+
+    return render(request, "view_blog.html", data)
