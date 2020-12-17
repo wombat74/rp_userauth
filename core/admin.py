6421ed0cbad2639cad9c15c5ad3b516c86409a0c
Eddie Gonzalez <Eddie@OfeliaGalezsMBP.fios-router.home>
2020-12-16 22:10:52 -0800

    added migrations and superuser

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
